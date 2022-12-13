92fd00ebdb43f284d9278cc1b865cd54fd519754
Marc Samtleben <marcsa1996@googlemail.com>
2022-12-13 11:33:22 +0100

    Seed admin user by env vars

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+if User.find_by(username: ENV.fetch("ADMIN_USERNAME")).blank?
+  User.create(username: ENV.fetch("ADMIN_USERNAME"), password: ENV.fetch("ADMIN_PASSWORD"))
+end

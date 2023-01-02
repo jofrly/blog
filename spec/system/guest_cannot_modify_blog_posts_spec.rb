ddee60303940937c1e0a2d20cdb5c54cb4277fab
Marc Samtleben <marcsa1996@googlemail.com>
2023-01-02 21:40:19 +0100

    Add all missing guest cannot modify blog posts specs

--- spec/system/guest_cannot_modify_blog_posts_spec.rb
+++ spec/system/guest_cannot_modify_blog_posts_spec.rb
@@ -5,14 +5,29 @@ RSpec.describe 'Guest cannot modify blog posts' do
     driven_by(:rack_test)
   end
 
-  it 'cannot visit the new route' do
+  it 'cannot access the new action' do
     visit new_blog_post_path
     expect(page).to have_current_path(root_path, ignore_query: true)
   end
 
-  it 'cannot visit the update route' do
+  it 'cannot access the create action' do
+    post blog_posts_path
+    expect(page).to have_current_path(root_path, ignore_query: true)
+  end
+
+  it 'cannot access the edit action' do
     post = create(:post)
     visit edit_blog_post_path(post)
+    expect(page).to have_current_path(root_path, ignore_query: true)
+  end
+
+  it 'cannot access the update action' do
+    patch blog_post_path('test')
+    expect(page).to have_current_path(root_path, ignore_query: true)
+  end
+
+  it 'cannot access the destroy action' do
+    delete blog_post_path('test')
     expect(page).to have_current_path(root_path, ignore_query: true)
   end
 end
